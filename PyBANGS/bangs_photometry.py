4a82c6065020a6b45995ec3d41db912040cc2e83
jacopo-chevallard <jacopo.chevallard@gmail.com>
2015-08-20 15:16:58 +0200

    Plot of marginal photometry correctly deals with arrays of zeros, fixes #1

--- PyBANGS/bangs_photometry.py
+++ PyBANGS/bangs_photometry.py
@@ -1,8 +1,10 @@
-
+import os
 from scipy.integrate import simps, cumtrapz
 from scipy.interpolate import interp1d
 from bisect import bisect_left
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
 from astropy.io import ascii
 from astropy.io import fits
 
@@ -73,7 +75,7 @@ class Photometry:
 
         # From the (previously loaded) observed catalogue select the row
         # corresponding to the input ID
-        observation = observed_catalogue.data[observed_catalogue.data['ID'] == ID]
+        observation = self.observed_catalogue.data[self.observed_catalogue.data['ID'] == ID]
 
         # Check if you need to apply an aperture correction to the catalogue fluxes
         if 'aper_corr' in self.observed_catalogue.data.dtype.names:
@@ -85,14 +87,14 @@ class Photometry:
         obs_flux = np.zeros(self.filters.n_bands)
         obs_flux_err = np.zeros(self.filters.n_bands)
 
-        for i, band in enumerate(self.filters['flux_colName']):
+        for i, band in enumerate(self.filters.data['flux_colName']):
             obs_flux[i] = observation[0][band]*aper_corr*self.filters.units / nanoJy
 
         # Add to the error array the minimum relative error thet BANGS allows
         # one to add to the errors quoted in the catalogue
-        for i, err in enumerate(self.filters['flux_errcolName']):
+        for i, err in enumerate(self.filters.data['flux_errcolName']):
             obs_flux_err[i] = observation[0][err]*aper_corr*self.filters.units / nanoJy
-            obs_flux_err[i] = np.sqrt( (obs_flux_err[i]/obs_flux[i])**2 + np.float32(self.filters['min_rel_err'][i])**2) * obs_flux[i]
+            obs_flux_err[i] = np.sqrt( (obs_flux_err[i]/obs_flux[i])**2 + np.float32(self.filters.data['min_rel_err'][i])**2) * obs_flux[i]
 
         ok = np.where(obs_flux_err > 0.)[0]
 
@@ -103,35 +105,51 @@ class Photometry:
         fits_file = os.path.join(BangsDirectories.results_dir, str(ID)+'_BANGS.fits.gz')
         hdulist = fits.open(fits_file)
 
-        # Consider onyl the extension containing the predicted model fluxes
+        # Consider only the extension containing the predicted model fluxes
         model_sed = hdulist['marginal photometry']
         probability = hdulist['posterior pdf'].data['probability']
 
         n_bands = len(model_sed.columns.names)
         median_flux = np.zeros(n_bands)
         pdf_norm = np.zeros(n_bands)
-        med = np.zeros(n_bands)
         _max_y = np.zeros(n_bands)
+        min_flux = np.zeros(n_bands)
+        max_flux = np.zeros(n_bands)
 
-        y_plot = list()
-        x_plot = list()
+        y_plot = list(range(n_bands))
+        x_plot = list(range(n_bands))
 
-        kde_pdf = list()
+        has_pdf = list(range(n_bands))
+        kde_pdf = list(range(n_bands))
         nXgrid = 1000
 
-        width = 5*np.min(np.array(filters.wl_eff[1:])-np.array(filters.wl_eff[0:-1]))
+        width = 5*np.min(np.array(self.filters.data['wl_eff'][1:])-np.array(self.filters.data['wl_eff'][0:-1]))
 
         kwargs = {'color':'tomato', 'alpha':0.7, 'edgecolor':'black', 'linewidth':0.2}
 
         for i in range(n_bands):
             xdata = model_sed.data.field(i) / nanoJy
+
+            if i == 0:
+                xdata[:] = 0.
+
             min_x = np.min(xdata)
             max_x = np.max(xdata)
 
             min_flux[i] = min_x
             max_flux[i] = max_x
 
+            # if min_x == max_x, then you can not use weighted KDE, since you
+            # just have one value for the x...this usually happens bacause of
+            # IGM absorption, which absorbs the flux blue-ward 1216 AA, making
+            # all flux = 0
+            if min_x == max_x:
+                has_pdf[i] = False
+                median_flux[i] = min_x
+                continue
+
             # Compute the marginal PDF through a weighted KDE
+            has_pdf[i] = True
             pdf = WeightedKDE.gaussian_kde(xdata, weights=probability)
 
             # Build a grid of value over which computing the actual PDF from its KDE
@@ -141,8 +159,8 @@ class Photometry:
             pdf_grid = np.array(pdf(x_grid))
 
             # Compute the PDF normalization, so its integral will be exactly 1
-            pdf_norm = simps(pdf_grid, x_grid)
-            pdf_grid /= pdf_norm
+            pdf_norm[i] = simps(pdf_grid, x_grid)
+            pdf_grid /= pdf_norm[i]
 
             # Now compute the cumulative PDF
             cumul_pdf = cumtrapz(pdf_grid, x_grid, initial = 0.)
@@ -158,20 +176,20 @@ class Photometry:
             prob_lims = pdf(lims) / pdf_norm[i]
 
             # The median corresponds to a cumulative probability = 0.5
-            med = f_interp(0.5)
+            median_flux[i] = f_interp(0.5)
 
-            kde_pdf.append(pdf)
+            kde_pdf[i] = pdf
 
             i1 = bisect_left(x_grid, lims[0])
             i2 = bisect_left(x_grid, lims[1])
         
-            x_plot.append(np.concatenate(([lims[0]], x_grid[i1+1:i2], [lims[1]])))
+            x_plot[i] = np.concatenate(([lims[0]], x_grid[i1+1:i2], [lims[1]]))
 
-            y_plot.append(np.concatenate(([prob_lims[0]], pdf_grid[i1+1:i2], [prob_lims[1]])))
+            y_plot[i] = np.concatenate(([prob_lims[0]], pdf_grid[i1+1:i2], [prob_lims[1]]))
 
             _max_y[i] = np.max(y_plot[i])
 
-        delta_wl = np.array(filters.wl_eff[1:])-np.array(filters.wl_eff[0:-1])
+        delta_wl = np.array(self.filters.data['wl_eff'][1:])-np.array(self.filters.data['wl_eff'][0:-1])
         delta_wl = np.concatenate(([delta_wl[0]], delta_wl))
 
         for i in range(n_bands):
@@ -180,26 +198,27 @@ class Photometry:
             if i > 1:
                 dwl = np.min(delta_wl[i-1:i])
 
-            w = 0.4 * dwl / _max_y[i]
+            if has_pdf[i]:
 
-            y_grid = np.full(len(x_plot[i]), filters.wl_eff[i])
+                w = 0.4 * dwl / _max_y[i]
 
-            _lim_y = kde_pdf[i](med[i])/pdf_norm[i] * w
-            
-            ax.fill_betweenx(x_plot[i],
-                    y_grid - y_plot[i]*w,
-                    y_grid + y_plot[i]*w,
-                    **kwargs
-                    )
+                y_grid = np.full(len(x_plot[i]), self.filters.data['wl_eff'][i])
+                _lim_y = kde_pdf[i](median_flux[i])/pdf_norm[i] * w
 
-            ax.plot( [filters.wl_eff[i]-_lim_y, filters.wl_eff[i]+_lim_y],
-                    [med[i], med[i]],
-                    color = 'black',
-                    linewidth = 0.2
-                    )
+                ax.fill_betweenx(x_plot[i],
+                        y_grid - y_plot[i]*w,
+                        y_grid + y_plot[i]*w,
+                        **kwargs
+                        )
 
-            ax.plot( filters.wl_eff[i],
-                    med[i],
+                ax.plot( [self.filters.data['wl_eff'][i]-_lim_y, self.filters.data['wl_eff'][i]+_lim_y],
+                        [median_flux[i], median_flux[i]],
+                        color = 'black',
+                        linewidth = 0.2
+                        )
+
+            ax.plot( self.filters.data['wl_eff'][i],
+                    median_flux[i],
                     color = 'black',
                     marker = "o",
                     markersize = 5,
@@ -212,13 +231,13 @@ class Photometry:
         if yMin > 0.:
             yMin *= 1.2
 
-        yMax = np.max(upper_flux)
+        yMax = np.max(max_flux)
         yMax *= 1.2
 
         ax.set_ylim([yMin, yMax])
 
-        x0 = filters.wl_eff[0]
-        x1 = filters.wl_eff[-1]
+        x0 = self.filters.data['wl_eff'][0]
+        x1 = self.filters.data['wl_eff'][-1]
         dx = x1-x0
         ax.set_xlim([x0-0.05*dx, x1+0.05*dx])
 
@@ -242,8 +261,8 @@ class Photometry:
             chi_square[i,:] +=  (obs_flux[i]/factor-xdata/factor)**2/(obs_flux_err[i]/factor)**2
 
         sum_chi_square = np.sum(chi_square[ok,:], axis=0)
-        self.aver_chi_square = np.sum(self.probability*sum_chi_square) / np.sum(self.probability)
-        self.aver_red_chi_square = self.aver_chi_square / (n_data-1)
+        #self.aver_chi_square = np.sum(self.probability*sum_chi_square) / np.sum(self.probability)
+        #self.aver_red_chi_square = self.aver_chi_square / (n_data-1)
 
         # Define plotting styles
         ax.set_xlabel("$\lambda_\\textnormal{eff} / \\textnormal{\AA}$ (observed-frame)")
@@ -258,7 +277,7 @@ class Photometry:
 
         kwargs = {'alpha':0.7}
 
-        plt.errorbar(filters.wl_eff, 
+        plt.errorbar(self.filters.data['wl_eff'], 
                 obs_flux, 
                 yerr = obs_flux_err,
                 color = "dodgerblue",
@@ -271,25 +290,26 @@ class Photometry:
 
 
         # Title of the plot is the object ID
-        plt.title(self.title)
+        plt.title(str(ID))
 
         # Print the evidence
         x0, x1 = ax.get_xlim()
         x = x0 + (x1-x0)*0.03
         y0, y1 = ax.get_ylim()
         y = y1 - (y1-y0)*0.10
-        ax.text(x, y, "$\log(Z)=" + "{:.2f}".format(self.logEvidence) + "$", fontsize=10 )
+        #ax.text(x, y, "$\log(Z)=" + "{:.2f}".format(self.logEvidence) + "$", fontsize=10 )
 
         # Print the average reduced chi-square
         y = y1 - (y1-y0)*0.15
-        ax.text(x, y, "$\langle\chi^2\\rangle=" + "{:.2f}".format(self.aver_chi_square) + "$", fontsize=10 )
+        #ax.text(x, y, "$\langle\chi^2\\rangle=" + "{:.2f}".format(self.aver_chi_square) + "$", fontsize=10 )
 
         y = y1 - (y1-y0)*0.20
-        ax.text(x, y, "$\langle\chi^2/(\\textnormal{N}_\\textnormal{data}-1)\\rangle=" + "{:.2f}".format(self.aver_red_chi_square) +"\; (\\textnormal{N}_\\textnormal{data}=" + "{:d}".format(n_data) + ")" + "$", fontsize=10 )
+       # ax.text(x, y, "$\langle\chi^2/(\\textnormal{N}_\\textnormal{data}-1)\\rangle=" + "{:.2f}".format(self.aver_red_chi_square) +"\; (\\textnormal{N}_\\textnormal{data}=" + "{:d}".format(n_data) + ")" + "$", fontsize=10 )
 
         if y0 < 0.: plt.plot( [x0,x1], [0.,0.], color='gray', lw=1.0 )
 
-        fig.savefig(self.outputfile_name + '_marginal_SED_phot.pdf', dpi=None, facecolor='w', edgecolor='w',
+        outputfile_name = os.path.join(BangsDirectories.results_dir, str(ID)+'_BANGS_marginal_SED_phot.pdf')
+        fig.savefig(outputfile_name, dpi=None, facecolor='w', edgecolor='w',
                 orientation='portrait', papertype='a4', format="pdf",
                 transparent=False, bbox_inches="tight", pad_inches=0.1)
         plt.close(fig)
@@ -314,7 +334,7 @@ class Photometry:
 ##            self.residual.load(self.residual_file_name)
 ##        except:
 ##            self.residual.compute(self.observed_catalogue,
-##                self.bangs_summary_catalogue, self.filters,
+##                self.bangs_summary_catalogue, self.self.filters.
 ##                cPickleName=self.residual_file_name)
                 
 

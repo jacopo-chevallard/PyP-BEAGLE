2227ec78a10ac97aae3c846a49b3234b1aec3328
jacopo-chevallard <jacopo.chevallard@gmail.com>
2017-03-30 14:38:03 +0200

    Added missing file to dependencies; added init module

---
+++ PyP-BEAGLE/dependencies/autoscale.py
@@ -0,0 +1,53 @@
+def autoscale_x(ax,margin=0.1):
+    """This function rescales the x-axis based on the data that is visible given the current ylim of the axis.
+    ax -- a matplotlib axes object
+    margin -- the fraction of the total height of the x-data to pad the upper and lower xlims"""
+
+    import numpy as np
+
+    def get_bottom_top(line):
+        xd = line.get_xdata()
+        yd = line.get_ydata()
+        lo,hi = ax.get_ylim()
+        x_displayed = xd[((yd>lo) & (yd<hi))]
+        h = np.max(x_displayed) - np.min(x_displayed)
+        bot = np.min(x_displayed)-margin*h
+        top = np.max(_displayed)+margin*h
+        return bot,top
+
+    lines = ax.get_lines()
+    bot,top = np.inf, -np.inf
+
+    for line in lines:
+        new_bot, new_top = get_bottom_top(line)
+        if new_bot < bot: bot = new_bot
+        if new_top > top: top = new_top
+
+    ax.set_xlim(bot,top)
+
+def autoscale_y(ax,margin=0.1):
+    """This function rescales the y-axis based on the data that is visible given the current xlim of the axis.
+    ax -- a matplotlib axes object
+    margin -- the fraction of the total height of the y-data to pad the upper and lower ylims"""
+
+    import numpy as np
+
+    def get_bottom_top(line):
+        xd = line.get_xdata()
+        yd = line.get_ydata()
+        lo,hi = ax.get_xlim()
+        y_displayed = yd[((xd>lo) & (xd<hi))]
+        h = np.max(y_displayed) - np.min(y_displayed)
+        bot = np.min(y_displayed)-margin*h
+        top = np.max(y_displayed)+margin*h
+        return bot,top
+
+    lines = ax.get_lines()
+    bot,top = np.inf, -np.inf
+
+    for line in lines:
+        new_bot, new_top = get_bottom_top(line)
+        if new_bot < bot: bot = new_bot
+        if new_top > top: top = new_top
+
+    ax.set_ylim(bot,top)

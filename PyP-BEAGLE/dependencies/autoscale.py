d3f236a46c6c3d88a578ea51454335a44fce956f
jacopo-chevallard <jacopo.chevallard@gmail.com>
2017-07-28 16:06:30 +0200

    Improved the autoscaling functions

--- PyP-BEAGLE/dependencies/autoscale.py
+++ PyP-BEAGLE/dependencies/autoscale.py
@@ -1,49 +1,92 @@
-def autoscale_x(ax,margin=0.1):
-    """This function rescales the x-axis based on the data that is visible given the current ylim of the axis.
+import numpy as np
+
+default_bot = np.inf
+default_top =  -np.inf
+
+def get_autoscale_y(ax, margin=0.1, ylog=False, top_margin=None, bottom_margin=None):
+    """
+    This function rescales the y-axis based on the data that is visible given the current xlim of the axis.
     ax -- a matplotlib axes object
-    margin -- the fraction of the total height of the x-data to pad the upper and lower xlims"""
+    margin -- the fraction of the total height of the y-data to pad the upper and lower ylims
+    top_margin -- the fraction of the total height of the y-data to pad the upper ylims
+    bottom_margin -- the fraction of the total height of the y-data to pad the upper ylims
+    """
 
-    import numpy as np
-
-    def get_bottom_top(line):
-        xd = line.get_xdata()
-        yd = line.get_ydata()
-        lo,hi = ax.get_ylim()
-        x_displayed = xd[((yd>lo) & (yd<hi))]
-        h = np.max(x_displayed) - np.min(x_displayed)
-        bot = np.min(x_displayed)-margin*h
-        top = np.max(_displayed)+margin*h
-        return bot,top
-
-    lines = ax.get_lines()
-    bot,top = np.inf, -np.inf
-
-    for line in lines:
-        new_bot, new_top = get_bottom_top(line)
-        if new_bot < bot: bot = new_bot
-        if new_top > top: top = new_top
-
-    ax.set_xlim(bot,top)
-
-def autoscale_y(ax,margin=0.1):
-    """This function rescales the y-axis based on the data that is visible given the current xlim of the axis.
-    ax -- a matplotlib axes object
-    margin -- the fraction of the total height of the y-data to pad the upper and lower ylims"""
-
-    import numpy as np
+    if top_margin is None: top_margin = margin
+    if bottom_margin is None: bottom_margin = margin
 
     def get_bottom_top(line):
         xd = line.get_xdata()
         yd = line.get_ydata()
         lo,hi = ax.get_xlim()
         y_displayed = yd[((xd>lo) & (xd<hi))]
-        h = np.max(y_displayed) - np.min(y_displayed)
-        bot = np.min(y_displayed)-margin*h
-        top = np.max(y_displayed)+margin*h
-        return bot,top
+        if len(y_displayed) > 0:
+            ymax = np.amax(y_displayed)
+            if ylog: ymax = np.log10(ymax)
+
+            ymin = np.amin(y_displayed)
+            if ylog: ymin = np.log10(ymin)
+
+            h = ymax - ymin
+
+            bot = ymin - bottom_margin*h
+            top = ymax + top_margin*h
+
+            if ylog: 
+                bot = 10.**bot
+                top = 10.**top
+        else:
+            bot, top = default_bot, default_top
+        return bot, top
 
     lines = ax.get_lines()
-    bot,top = np.inf, -np.inf
+    bot, top = default_bot, default_top
+
+    for line in lines:
+        new_bot, new_top = get_bottom_top(line)
+        if new_bot < bot: bot = new_bot
+        if new_top > top: top = new_top
+
+    return [bot,top]
+
+def autoscale_y(ax, margin=0.1, ylog=False, top_margin=None, bottom_margin=None):
+    """
+    This function rescales the y-axis based on the data that is visible given the current xlim of the axis.
+    ax -- a matplotlib axes object
+    margin -- the fraction of the total height of the y-data to pad the upper and lower ylims
+    top_margin -- the fraction of the total height of the y-data to pad the upper ylims
+    bottom_margin -- the fraction of the total height of the y-data to pad the upper ylims
+    """
+
+    if top_margin is None: top_margin = margin
+    if bottom_margin is None: bottom_margin = margin
+
+    def get_bottom_top(line):
+        xd = line.get_xdata()
+        yd = line.get_ydata()
+        lo,hi = ax.get_xlim()
+        y_displayed = yd[((xd>lo) & (xd<hi))]
+        if len(y_displayed) > 0:
+            ymax = np.amax(y_displayed)
+            if ylog: ymax = np.log10(ymax)
+
+            ymin = np.amin(y_displayed)
+            if ylog: ymin = np.log10(ymin)
+
+            h = ymax - ymin
+
+            bot = ymin - bottom_margin*h
+            top = ymax + top_margin*h
+
+            if ylog: 
+                bot = 10.**bot
+                top = 10.**top
+        else:
+            bot, top = default_bot, default_top
+        return bot, top
+
+    lines = ax.get_lines()
+    bot, top = default_bot, default_top
 
     for line in lines:
         new_bot, new_top = get_bottom_top(line)

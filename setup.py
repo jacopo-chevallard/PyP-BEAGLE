1d1ac04bd22542c4283ea1ff0698e28485274e99
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2018-07-06 12:29:36 +0200

    Added dependency on 'natsort' package for natural sorting

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['atpy', 'getdist', 'pathos', 'astropy', 'bokeh'],
+    install_requires=['atpy', 'getdist', 'pathos', 'astropy', 'bokeh', 'natsort'],
 
     include_package_data=True, 
 

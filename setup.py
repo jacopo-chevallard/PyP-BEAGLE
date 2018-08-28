e8a48edd8dd67586a18d2b8341364be448943a3b
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2018-08-28 12:40:06 +0200

    Specifying astropy version, otherwise the installation will fail, closes #33

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib', 'scipy', 'numpy', 'atpy', 'getdist', 'pathos', 'astropy', 'bokeh', 'natsort'],
+    install_requires=['matplotlib', 'scipy', 'numpy', 'atpy', 'getdist', 'pathos', 'astropy<3', 'bokeh', 'natsort'],
 
     include_package_data=True, 
 
